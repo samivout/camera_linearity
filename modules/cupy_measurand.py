2ee32594ee99441a914cc9063848074c0c4b0fff
samivout <124705587+samivout@users.noreply.github.com>
2025-01-29 11:01:16 +0200

    Accept NumPy arrays as input, fix gkde method.

--- modules/cupy_measurand.py
+++ modules/cupy_measurand.py
@@ -11,8 +11,9 @@ from typing import Optional, Union, List
 from typing import Union
 from typing import List
 from global_settings import GlobalSettings as gs
-from cupyx.scipy.ndimage import median_filter
+from cupyx.scipy.ndimage import median_filter as cp_median_filter
 from scipy.stats import gaussian_kde
+import numpy as np
 
 from measurand import AbstractMeasurand, ScalarType
 
@@ -24,13 +25,16 @@ except ImportError:
 ScalarType = Union[int, float]
 
 
-class CuPyMeasurand(AbstractMeasurand):
+class CupyMeasurand(AbstractMeasurand):
     """
-    CuPy version of the base AbstractMeasurand class.
+    CuPy version of the base AbstractMeasurand class. CupyMeasurand allows assigning NumPy arrays into val and std
+    instance attributes by doing a silent conversion into a CuPy array. The equivalent case is not true for
+    NumpyMeasurand and instead leads to a ValueError, following CuPy convention of not allowing implicit conversion.
     """
 
     lib = cp
-    fn_median_filter = median_filter
+    backend = "cupy"
+    fn_median_filter = cp_median_filter
     ArrayType = cp.ndarray
     InputType = Union[ScalarType, ArrayType]
 
@@ -60,20 +64,26 @@ class CuPyMeasurand(AbstractMeasurand):
         return self._val
 
     @val.setter
-    def val(self, value: Optional[cp.ndarray]):
-        if value is not None and not isinstance(value, self.ArrayType):
+    def val(self, value: Optional[Union[np.ndarray, cp.ndarray]]):
+        if value is not None and isinstance(value, np.ndarray):
+            self._val = cp.array(value)
+        elif value is None or isinstance(value, cp.ndarray):
+            self._val = value
+        else:
             raise TypeError(f"val must be an array or None, got {type(value)} instead.")
-        self._val = value
 
     @property
     def std(self):
         return self._std
 
     @std.setter
-    def std(self, value: Optional[cp.ndarray]):
-        if value is not None and not isinstance(value, self.ArrayType):
+    def std(self, value: Optional[Union[np.ndarray, cp.ndarray]]):
+        if value is not None and isinstance(value, np.ndarray):
+            self.std = cp.array(value)
+        elif value is None or isinstance(value, cp.ndarray):
+            self.std = value
+        else:
             raise TypeError(f"std must be an array or None, got {type(value)} instead.")
-        self._std = value
 
     def compute_kernel_density_estimate(self, data_points: int, included_range: Optional[tuple[float, float]] = None,
                                         channels: Optional[List[int]] = None, use_std: Optional[bool] = False):
@@ -97,12 +107,12 @@ class CuPyMeasurand(AbstractMeasurand):
         for c in channels:
 
             channel_values = self.val[..., c]
-            finite_mask = cnp.isfinite(channel_values)
+            finite_mask = cp.isfinite(channel_values)
 
             if use_std:
                 stds = self.std[..., c]
                 non_zeros = stds != 0
-                finite_mask = cnp.logical_and(finite_mask, non_zeros)
+                finite_mask = cp.logical_and(finite_mask, non_zeros)
                 weights = stds[finite_mask]
                 weights = 1 / weights
                 channel_values = channel_values[finite_mask]
@@ -110,10 +120,9 @@ class CuPyMeasurand(AbstractMeasurand):
                 weights = None
                 channel_values = channel_values[finite_mask]
 
-            if using_cupy:
-                channel_values = cp.asnumpy(channel_values)
-                if weights is not None:
-                    weights = cp.asnumpy(weights)
+            channel_values = cp.asnumpy(channel_values)
+            if weights is not None:
+                weights = cp.asnumpy(weights)
 
             if included_range is None:
                 x_range = np.linspace(np.min(channel_values), np.max(channel_values), num=data_points)
