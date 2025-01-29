14223bef275b0513490a60c9e3939d25cae9cb22
samivout <124705587+samivout@users.noreply.github.com>
2025-01-29 10:59:22 +0200

    Remove unnecessary filter function getter.

--- modules/cupy_wrapper.py
+++ modules/cupy_wrapper.py
@@ -20,19 +20,3 @@ def get_array_libraries():
         using_cupy = False
 
     return np, cp, using_cupy
-
-
-def get_filter_functions():
-    """
-    Getter for setting up use of filter functions on either CuPy or SciPy, based on availability of CuPy.
-    Returns:
-        Either CuPy or SciPy implementation of the gaussian and median filters.
-    """
-    try:
-        from cupyx.scipy.ndimage import gaussian_filter
-        from cupyx.scipy.ndimage import median_filter
-    except ImportError:
-        from scipy.ndimage import gaussian_filter
-        from scipy.ndimage import median_filter
-
-    return gaussian_filter, median_filter
